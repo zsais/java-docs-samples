0ecc51ba4758cbb40d55228ae2d54cd3eb64c43f
Zach Sais <zach.sais@utexas.edu>
2022-12-09 11:24:59 -0500

    remove keys

--- run/idp-sql/src/main/resources/static/config.js
+++ run/idp-sql/src/main/resources/static/config.js
@@ -1,7 +1,7 @@
 // [START cloudrun_end_user_firebase_config]
 var config = {
-  apiKey: "AIzaSyAaveWY8KV6zJLV4gSLDHTVXblutTmAorI",
-  authDomain: "zanthos-22.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
 };
 firebase.initializeApp(config);
 // [END cloudrun_end_user_firebase_config]
